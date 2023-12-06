6eb04f9dd4a4d66eb8bef7c0eee2968d8f3ad3d9
Diego Priess <diegopriess.dev@gmail.com>
2023-12-05 23:58:30 -0300

    Alterando cors

--- src/main/java/com/ifmatch/ifmatchservice/config/CorsConfig.java
+++ src/main/java/com/ifmatch/ifmatchservice/config/CorsConfig.java
@@ -11,6 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/user").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/user").allowedOrigins("https://ifmatch-app.onrender.com");
     }
 }
